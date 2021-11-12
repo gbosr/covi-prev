dff848b71c136fb6a56cbdf4a95251b0d9e6710f
gbosr <gaspard@getsmarto.com>
2021-11-12 20:38:03 +0100

    design changes /page-0
    Added main.css + few changes in app.py to accept css classes

--- app.py
+++ app.py
@@ -26,38 +26,36 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
 import plotly.graph_objs as go
-
-data = pd.read_csv('data/coviprev-age.csv',sep=';',decimal=',')
+data = pd.read_csv('data/coviprev-age.csv', sep=';', decimal=',')
 
 
 # ANXIETY
 # Create the line graph
 line_graph_anxiety = px.line(
-  # Set the appropriate DataFrame and title
-  data_frame=data[5:79], title='Evolution anxiete par age', 
-  # Set the x and y arguments
-  x='semaine', y='depression', 
-  # Ensure a separate line per country
-  color='age')
+    # Set the appropriate DataFrame and title
+    data_frame=data[5:79], title='Evolution anxiete par age',
+    # Set the x and y arguments
+    x='semaine', y='depression',
+    # Ensure a separate line per country
+    color='age')
 
-#plot(line_graph_anxiety)
-
+# plot(line_graph_anxiety)
 
 
 # DEPRESSION
 
 # Create the line graph
 line_graph_depression = px.line(
-  # Set the appropriate DataFrame and title
-  data_frame=data[5:79], title='Evolution de symptômes dépressifs par âge durant les vagues Covid', 
-  # Set the x and y arguments
-  x='semaine', y='depression', 
-  # Ensure a separate line per age
-  color='age',
-  labels={
-                     "semaine": "Vague",
-                     "depression": "% d'individus déclarant des symptômes dépressifs"
-                 })
+    # Set the appropriate DataFrame and title
+    data_frame=data[5:79], title='Evolution de symptômes dépressifs par âge durant les vagues Covid',
+    # Set the x and y arguments
+    x='semaine', y='depression',
+    # Ensure a separate line per age
+    color='age',
+    labels={
+      "semaine": "Vague",
+      "depression": "% d'individus déclarant des symptômes dépressifs"
+    })
 
 line_graph_depression.update_yaxes(range=[0, 40])
 
@@ -74,20 +72,18 @@ line_graph_depression.update_xaxes(title_font_family="Arial")
 picker_style = {'float': 'left', 'margin': 'auto'}
 
 
-#plot(line_graph_depression)
+# plot(line_graph_depression)
 
 
-
-
-#SLEEP
+# SLEEP
 # Create the line graph
 line_graph_sleep = px.line(
-  # Set the appropriate DataFrame and title
-  data_frame=data[5:79], title='Evolution problèmes de sommeil par age', 
-  # Set the x and y arguments
-  x='semaine', y='pbsommeil', 
-  # Ensure a separate line per country
-  color='age')
+    # Set the appropriate DataFrame and title
+    data_frame=data[5:79], title='Evolution problèmes de sommeil par age',
+    # Set the x and y arguments
+    x='semaine', y='pbsommeil',
+    # Ensure a separate line per country
+    color='age')
 
 
 line_graph_sleep.update_layout(
@@ -101,18 +97,18 @@ line_graph_sleep.update_layout(
 )
 
 
-#plot(line_graph_sleep)
+# plot(line_graph_sleep)
 
-#SEX
-data_sex = pd.read_csv('data/coviprev-sexe.csv',sep=';',decimal=',')
+# SEX
+data_sex = pd.read_csv('data/coviprev-sexe.csv', sep=';', decimal=',')
 # Create the line graph
-line_graph_sex_sommeil= px.line(
-  # Set the appropriate DataFrame and title
-  data_frame=data_sex[0:32], title='Evolution problèmes de sommeil par sexe', 
-  # Set the x and y arguments
-  x='semaine', y='pbsommeil', 
-  # Ensure a separate line per country
-  color='sexe')
+line_graph_sex_sommeil = px.line(
+    # Set the appropriate DataFrame and title
+    data_frame=data_sex[0:32], title='Evolution problèmes de sommeil par sexe',
+    # Set the x and y arguments
+    x='semaine', y='pbsommeil',
+    # Ensure a separate line per country
+    color='sexe')
 
 
 line_graph_sex_sommeil.update_layout(
@@ -125,13 +121,13 @@ line_graph_sex_sommeil.update_layout(
     plot_bgcolor='#ffffff'
 )
 
-line_graph_sex_depression= px.line(
-  # Set the appropriate DataFrame and title
-  data_frame=data_sex[0:32], title='Evolution problèmes de dépression par sexe', 
-  # Set the x and y arguments
-  x='semaine', y='depression', 
-  # Ensure a separate line per country
-  color='sexe')
+line_graph_sex_depression = px.line(
+    # Set the appropriate DataFrame and title
+    data_frame=data_sex[0:32], title='Evolution problèmes de dépression par sexe',
+    # Set the x and y arguments
+    x='semaine', y='depression',
+    # Ensure a separate line per country
+    color='sexe')
 
 
 line_graph_sex_depression.update_layout(
@@ -149,15 +145,15 @@ picker_style = {'float': 'left', 'margin': 'auto'}
 #####
 
 all_data = pd.read_csv('data/coviprev.csv',
-                       sep=';',decimal=',',header= 0,
-                        encoding= 'unicode_escape')
+                       sep=';', decimal=',', header=0,
+                       encoding='unicode_escape')
 
 
-fig_all = px.line(all_data, x='semaine', y=['depression', 'anxiete',"pbsommeil"],
-                    labels={
-                     "semaine": "Vague",
-                     "depression": "% d'individus déclarant des symptômes"
-                 })
+fig_all = px.line(all_data, x='semaine', y=['depression', 'anxiete', "pbsommeil"],
+                  labels={
+    "semaine": "Vague",
+    "depression": "% d'individus déclarant des symptômes"
+})
 
 fig_all.update_layout(
     font_family="Arial",
@@ -175,48 +171,58 @@ picker_style = {'float': 'left', 'margin': 'auto'}
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
+# app = dash.Dash(__name__)
 
 
 server = app.server
 # styling the sidebar
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "20rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
+# SIDEBAR_STYLE = {
+#     "position": "fixed",
+#     "top": 0,
+#     "left": 0,
+#     "bottom": 0,
+#     "width": "20rem",
+#     "padding": "2rem 1rem",
+#     "background-color": "#f8f9fa",
+# }
 
 # padding for the page content
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
+# CONTENT_STYLE = {
+#     "margin-left": "18rem",
+#     "margin-right": "2rem",
+#     "padding": "2rem 1rem",
+#     "background": "blue",
+# }
 
 sidebar = html.Div(
     [
-        html.H5("CoviPrev", className="display-4"),
-        html.Hr(),
-        html.P(
-            "Santé Publique France", className="lead"
-        ),
+        html.H1("Hippo", className="navbar-title"),
+        html.H5("CoviPrev", className="navbar-subtitle"),
         dbc.Nav(
             [
-                dbc.NavLink("Accueil", href="/", active="exact"),
-                dbc.NavLink("Dépression", href="/page-1", active="exact"),
-                dbc.NavLink("Sommeil", href="/page-2", active="exact"),
+                dbc.NavLink("Accueil", href="/", active="exact",
+                            className="text-navbar"),
+                dbc.NavLink("Dépression", href="/page-1",
+                            active="exact", className="text-navbar"),
+                dbc.NavLink("Sommeil", href="/page-2",
+                            active="exact", className="text-navbar"),
             ],
+            # html.Hr(),
+            # html.P(
+            #     "Santé Publique France", className="lead"
+            # ),
+
             vertical=True,
             pills=True,
+            className="summary-navbar",
         ),
     ],
-    style=SIDEBAR_STYLE,
+    # style=SIDEBAR_STYLE,
+    className="navbar",
 )
 
-content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+content = html.Div(id="page-content", children=[], className="content")
+# style=CONTENT_STYLE)
 
 app.layout = html.Div([
     dcc.Location(id="url"),
@@ -232,63 +238,56 @@ app.layout = html.Div([
 def render_page_content(pathname):
     if pathname == "/":
         return [
-                html.H1('',
-                        style={'textAlign':'center'}),
-                html.Div([
-    dcc.Markdown('''
-#### Dashboard #1 CoviPrev 📈
+            html.H1('Dashboard #1 CoviPrev 📈', className="content-title"),
+            html.P("Une enquête pour suivre l’évolution des comportements et de la santé mentale pendant l'épidémie de COVID-19. Depuis le 23 mars 2020, Santé publique France a lancé l'enquête CoviPrev en population générale afin de suivre l’évolution des comportements (gestes barrières, confinement, consommation d’alcool et de tabac, alimentation et activité physique) et de la santé mentale (bien-être, troubles).",
+                   className='content-paraf'),
+            html.P("Source de l'enquête ici", className="content-warning"),
+            # (https://www.santepubliquefrance.fr/etudes-et-enquetes/coviprev-une-enquete-pour-suivre-l-evolution-des-comportements-et-de-la-sante-mentale-pendant-l-epidemie-de-covid-19)
 
-**Une enquête pour suivre l’évolution des comportements et de la santé mentale pendant l'épidémie de COVID-19**
->
-> Depuis le 23 mars 2020, Santé publique France a lancé l'enquête CoviPrev en population générale afin de suivre l’évolution des comportements (gestes barrières, confinement, consommation d’alcool et de tabac, alimentation et activité physique) et de la santé mentale (bien-être, troubles).
->
+            #             html.Div([dcc.Markdown('''
+            # #### Objectifs ✔️
+            # * Suivre l’adoption des mesures de protection et de la santé de la population pendant la période de confinement et de déconfinement
+            # * Recueillir les informations nécessaires à l’orientation et à l’ajustement des mesures de prévention
+            # * Surveiller les inégalités de santé
+            # * Capitaliser des connaissances utiles à la gestion de futures pandémies
 
-_Source de l'enquête [suivant ce lien](https://www.santepubliquefrance.fr/etudes-et-enquetes/coviprev-une-enquete-pour-suivre-l-evolution-des-comportements-et-de-la-sante-mentale-pendant-l-epidemie-de-covid-19)._
+            # ''', style={'textAlign': 'left', 'margin-left': 100, 'width': '80%'})]),
 
-''', style={'textAlign':'center','margin-left': 100,'width': '80%'} )]),
- 
-html.Div([dcc.Markdown('''
-#### Objectifs ✔️
-* Suivre l’adoption des mesures de protection et de la santé de la population pendant la période de confinement et de déconfinement 
-* Recueillir les informations nécessaires à l’orientation et à l’ajustement des mesures de prévention
-* Surveiller les inégalités de santé
-* Capitaliser des connaissances utiles à la gestion de futures pandémies  
-
-''', style={'textAlign':'left','margin-left': 100,'width': '80%'} )]),
-
-html.Div([dcc.Markdown('''
-#### Méthodes 💡
-* Enquêtes quantitatives répétées sur échantillons indépendants
-* Questionnaires auto-administrés à remplir en ligne sur système Cawi (Computer Assisted Web Interview)
-* Echantillons de 2 000 personnes de 18 ans et plus résidant en France métropolitaine recrutés par access panel (Access Panel BVA)
-* Échantillonnage par quotas (sexe, âge, catégorie socio-professionnelles du répondant, région, catégorie d’agglomération) redressé sur le recensement général de la population 2016 
+            #             html.Div([dcc.Markdown('''
+            # #### Méthodes 💡
+            # * Enquêtes quantitatives répétées sur échantillons indépendants
+            # * Questionnaires auto-administrés à remplir en ligne sur système Cawi (Computer Assisted Web Interview)
+            # * Echantillons de 2 000 personnes de 18 ans et plus résidant en France métropolitaine recrutés par access panel (Access Panel BVA)
+            # * Échantillonnage par quotas (sexe, âge, catégorie socio-professionnelles du répondant, région, catégorie d’agglomération) redressé sur le recensement général de la population 2016
 
 
-''', style={'textAlign':'left','margin-left': 100,'width': '80%'} ),
-                 dcc.Graph(id='bargraph',
-                         figure=fig_all,
-                             style={'textAlign':'center','margin-left': 100,'width': '80%'}),
-                ])]
-                       
-                               
+            # ''', style={'textAlign': 'left', 'margin-left': 100, 'width': '80%'}),
+            #                 dcc.Graph(id='bargraph',
+            #                           figure=fig_all,
+            #                              style={'textAlign': 'center', 'margin-left': 100, 'width': '80%'}),
+            #             ])]
+            dcc.Graph(id='bargraph', figure=fig_all,
+                      className="content-graph"),
+        ]
+
     elif pathname == "/page-1":
         return [
-                html.H1('Données relatives aux signes dépressifs',
-                        style={'textAlign':'center'}),
-                dcc.Graph(id='bargraph',
-                         figure=line_graph_depression,style={'textAlign':'center','margin-left': 100,'width': '80%'}),
-                dcc.Graph(id='bargraph',
-                         figure=line_graph_sex_depression,style={'textAlign':'center','margin-left': 100,'width': '80%'})
-                ]
+            html.H1('Données relatives aux signes dépressifs',
+                    style={'textAlign': 'center'}),
+            dcc.Graph(id='bargraph',
+                         figure=line_graph_depression, style={'textAlign': 'center', 'margin-left': 100, 'width': '80%'}),
+            dcc.Graph(id='bargraph',
+                         figure=line_graph_sex_depression, style={'textAlign': 'center', 'margin-left': 100, 'width': '80%'})
+        ]
     elif pathname == "/page-2":
         return [
-                html.H1('Données relatives aux troubles du sommeil',
-                        style={'textAlign':'center'}),
-                dcc.Graph(id='bargraph',
-                         figure=line_graph_sleep,style={'textAlign':'center','margin-left': 100,'width': '80%'}),
-                dcc.Graph(id='bargraph',
-                         figure=line_graph_sex_sommeil,style={'textAlign':'center','margin-left': 100,'width': '80%'})
-                ]
+            html.H1('Données relatives aux troubles du sommeil',
+                    style={'textAlign': 'center'}),
+            dcc.Graph(id='bargraph',
+                         figure=line_graph_sleep, style={'textAlign': 'center', 'margin-left': 100, 'width': '80%'}),
+            dcc.Graph(id='bargraph',
+                         figure=line_graph_sex_sommeil, style={'textAlign': 'center', 'margin-left': 100, 'width': '80%'})
+        ]
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
@@ -299,9 +298,5 @@ html.Div([dcc.Markdown('''
     )
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run_server(debug=True)
-    
-    
-    
-    
